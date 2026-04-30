781a438e9cc4eb66572faa97999b99b28c352b15
mvdevelop <marcosvmdilly@gmail.com>
2026-04-30 12:00:43 -0300

    feat: working on generate

--- reactjs/src/pages/Generate.tsx
+++ reactjs/src/pages/Generate.tsx
@@ -1,10 +1,19 @@
 
-
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import type { IThumbnail } from "../assets/assets";
 
 const Generate = () => {
+
+  const { id } = useParams();
+  const [ title, setTitle ] = useState("");
+  const [ additionalDetails, setAdditionalDetails ] = useState("");
+
+  const [ thumbnail, setThumbnail ] = useState<IThumbnail | null>(null);
+
   return (
     <div>
-      Generate
+
     </div>
   )
 }
